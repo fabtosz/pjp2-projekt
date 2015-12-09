6eaca2bb68be729d69049203807e5bef1b690d44
fabtosz <bartosz.fabianski@outlook.com>
2015-12-09 08:32:50 +0100

    TYDZIEŃ 4 - KOLIZJE Z BLOCZKAMI

--- pjp2-projekt/Source.cpp
+++ pjp2-projekt/Source.cpp
@@ -3,12 +3,66 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+#include "objects.h"
+#include <math.h>
+
+#include <iostream>
+using namespace std;
 
 const int WIDTH = 600;
 const int HEIGHT = 500;
 
 bool keys[] = { false, false, false, false };
 enum KEYS{ UP, DOWN, LEFT, RIGHT };
+
+//ogólne sprawdzenie kolizji
+//sprawdza czy dwa prostok¹ty na siebie nachodz¹
+bool isCollide(float pos1_x, float pos1_y, float size1_x, float size1_y,
+	float pos2_x, float pos2_y, float size2_x, float size2_y)
+{
+	float sr1_x = pos1_x + size1_x / 2;
+	float sr1_y = pos1_y + size1_y / 2;
+
+	float sr2_x = pos2_x + size2_x / 2;
+	float sr2_y = pos2_y + size2_y / 2;
+
+	if (
+		abs(sr1_x - sr2_x) < abs((size1_x + size2_x) / 2) &&
+		abs(sr1_y - sr2_y) < abs((size1_y + size2_y) / 2)
+		)
+		return true;
+
+	return false;
+
+}
+
+// Sprawdzenie kolizji gracza z bloczkiem
+// u¿ycie isCollide z konkretnymi rozmiarami prostok¹tów
+bool isCollidePlayerTile(float player_x, float player_y, float tile_x, float tile_y)
+{
+	return isCollide(player_x, player_y, playerSizeX, playerSizeY, tile_x, tile_y, tileSize, tileSize);
+}
+
+bool canItMove(float player_x, float player_y,  // wspó³rzêdne gracza
+	float x, float y)				        // o ile ruszyæ
+{
+	bool collision = false;
+
+	//sprawdz kolizje dla ka¿dego kloca
+	for (int i = 0; i < sizeArrayMap; i++)
+	{
+		//jeœli klocek jest powietrzem to go pomiñ
+		if (map[i] != 0)
+		{
+			if (isCollidePlayerTile(player_x + x, player_y + y, tileSize * (i % mapColumns), tileSize * (i / mapColumns)))
+			{
+				collision = true;
+				break;
+			}
+		}
+	}
+	return !collision;
+}
 
 int main(void)
 {
@@ -18,21 +72,6 @@ int main(void)
 
 	int xOff = 0;
 	int yOff = 0;
-
-	int mapColumns = 20;
-	int mapSize = 200;
-	int tileSize = 50;
-
-	int map[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		          0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0,
-		          2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 1, 1, 0, 0, 0, 2, 2, 2,
-		          1, 2, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1,
-		          1, 1, 2, 2, 1, 1, 1, 1, 0, 0, 0, 2, 1, 1, 0, 0, 0, 1, 1, 1,
-		          1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1 };
 
 	// zmienne sprite'a
 	const int maxFrame = 6;
@@ -47,7 +86,7 @@ int main(void)
 	ALLEGRO_BITMAP *bgTiles = NULL;
 	ALLEGRO_BITMAP *bgClouds = NULL;
 	ALLEGRO_BITMAP *sprite[maxFrame];
-	ALLEGRO_BITMAP *splash;
+	//	ALLEGRO_BITMAP *splash;
 
 	//program init
 	if (!al_init())										//initialize Allegro
@@ -69,7 +108,13 @@ int main(void)
 	bgTiles = al_load_bitmap("tiles.gif");
 	bgClouds = al_load_bitmap("clouds.png");
 
-	// sprite
+	// gracz
+	Player player;
+
+	// Wspó³rzêdne pocz¹tkowe gracza
+	player.x = 200;
+	player.y = 200;
+
 	sprite[0] = al_load_bitmap("00.gif");
 	sprite[1] = al_load_bitmap("01.gif");
 	sprite[2] = al_load_bitmap("02.gif");
@@ -87,12 +132,13 @@ int main(void)
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 
+	/*
 	splash = al_load_bitmap("splash.jpg");
 	al_draw_bitmap(splash, 0, 0, 0);
 	al_flip_display();
 	al_rest(3.0);
 	al_destroy_bitmap(splash);
-
+	*/
 	al_start_timer(timer);
 	while (!done)
 	{
@@ -151,9 +197,53 @@ int main(void)
 				frameCount = 0;
 			}
 
-			xOff -= keys[RIGHT] * 5;
-			xOff += keys[LEFT] * 5;
+			// tu jest zmieniana pozycja grcza
+			// jeœli ruch wywo³a zderzenie z drugim obiektem to nie jest wykonywany 
+			if (keys[RIGHT])
+			{
+				if (canItMove(player.x, player.y, 5, 0))
+				{
+					player.x += 5;
+				}
+			}
 
+			if (keys[LEFT])
+			{
+				if (canItMove(player.x, player.y, -5, 0))
+				{
+					player.x -= 5;
+				}
+			}
+
+			if (keys[DOWN])
+			{
+				if (canItMove(player.x, player.y, 0, 5))
+				{
+					player.y += 5;
+				}
+			}
+
+			if (keys[UP])
+			{
+				if (canItMove(player.x, player.y, 0, -5))
+				{
+					player.y -= 5;
+				}
+			}
+
+			//przeliczenie wspó³rzêdnych mapy
+			//gracz z prawej
+			if (player.x < WIDTH / 2) {
+				xOff = 0;
+			}
+			//gracz w œrodku
+			if (WIDTH / 2 < player.x  && player.x < mapWidth - WIDTH / 2) {
+				xOff = -(player.x - WIDTH / 2);
+			}
+			//gracz z lewej
+			if (mapWidth - WIDTH / 2 < player.x) {
+				xOff = -(mapWidth - WIDTH);
+			}
 
 			render = true;
 		}
@@ -162,15 +252,18 @@ int main(void)
 		{
 			render = false;
 
+			//rysuj tlo
 			al_draw_bitmap(bgClouds, 0, 0, 0);
 
-			for (int i = 0; i < mapSize; i++)
+			//rysuj bloczki
+			for (int i = 0; i < sizeArrayMap; i++)
 			{
 				al_draw_bitmap_region(bgTiles, tileSize * map[i], 0, tileSize, tileSize,
 					xOff + tileSize * (i % mapColumns), yOff + tileSize * (i / mapColumns), 0);
 			}
 
-			al_draw_bitmap(sprite[curFrame], 200 + xOff, 210, 0);
+			//rysuj gracza
+			al_draw_bitmap(sprite[curFrame], player.x + xOff, player.y + yOff, 0);
 
 			al_flip_display();
 			al_clear_to_color(al_map_rgb(0, 0, 0));
